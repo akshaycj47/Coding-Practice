119a83d6fed4130689bb31114d25c056a6030711
Akshay Sawant <asawant@gatech.edu>
2016-05-10 22:08:28 -0400

    Adding insertion sort

---
+++ CLRS/2. Getting Started/2.1_InsertionSort.cpp
@@ -0,0 +1,20 @@
+// Sort an array of n numbers in the ascending order
+
+#include <iostream>
+using namespace std;
+
+/**
+ * Function which sorts an array of n numbers in the ascending order
+ * @params {array} arr - The array which we have to sort
+ * @params {int} n - Size of the array
+ */
+void insertionSort(int* arr, int n) {
+
+}
+
+/**
+ * Starting point of the program
+ */
+int main() {
+
+}
