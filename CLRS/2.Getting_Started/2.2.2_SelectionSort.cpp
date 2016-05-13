9dbb703a2003b0c38b4c926089f904e78c6e6413
Akshay Sawant <asawant@gatech.edu>
2016-05-12 21:59:18 -0400

    Added selection sort

---
+++ CLRS/2.Getting_Started/2.2.2_SelectionSort.cpp
@@ -0,0 +1,43 @@
+// Sort an array of n numbers in the ascending order
+// CLRS page 29
+
+#include <iostream>
+#include <cassert>
+#include <climits>
+using namespace std;
+
+/**
+ * Function which sorts an array of n numbers in the ascending order
+ * @params {array} arr - The array which we have to sort
+ * @params {int} n - Size of the array
+ */
+void selectionSort(int* arr, int n) {
+
+	for (int i = 0; i < n; i++) {
+		int min = INT_MAX;
+		int minIndex = -1;
+		for (int j = i; j < n; j++) {
+			if (arr[j] < min) {
+				min = arr[j];
+				minIndex = j;
+			} 
+		}
+		int temp = arr[i];
+		arr[i] = arr[minIndex];
+		arr[minIndex] = temp;
+	}
+}
+
+/**
+ * Starting point of the program
+ */
+int main() {
+
+	int arr[] = {31, 41, 59, 26, 41, 58};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	selectionSort(arr, n);
+	int res[] = {26, 31, 41, 41, 58, 59};
+	for (int i = 0; i < n; i++) {
+		assert(arr[i] == res[i]);
+	}
+}
