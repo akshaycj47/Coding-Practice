6783984cd96cbf534b296c73f7f0fd5aedbd78be
Akshay Sawant <asawant@gatech.edu>
2016-12-01 21:48:45 -0800

    Start backtracking

---
+++ G4G/Algo/Backtracking/PermutationStringIterative.cpp
@@ -0,0 +1,52 @@
+#include <cstring>
+#include <iostream>
+
+/**
+* Function which swaps two characters
+* @params {char} x - First character
+* @params {char} y - Second character
+*/
+void swap(char* x, char* y) {
+
+	char temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+/**
+* Function which prints all the permutations of a given string
+* @params {char array} c - Given string
+* @params {int} n - Length of the string
+*/
+void printPermutationsString(char* c, int n) {
+
+	for (int i = 0; i < n; i++) {
+
+		// Initial swap
+		swap(c + 0, c + i);
+
+		// For all the remaining elements except fixed element
+		for (int j = 0; j < n - 1; j++) {
+
+			// Shifting for loop
+			for (int k = 1; k < n - 1; k++) {
+				swap(c + k, c + k + 1);					// Shifting 1st element one by one till it reaches the end
+				std::cout << c << std::endl;
+			}
+		}
+	}
+}
+
+/**
+* Starting point of the program
+*/
+int main() {
+
+	char c[] = "ABC";
+	int n = strlen(c);
+	printPermutationsString(c, n);
+
+	char c2[] = "ABCD";
+	int n2 = strlen(c2);
+	printPermutationsString(c2, n2);
+}
---
+++ G4G/Algo/Backtracking/PermutationStringRecursive.cpp
@@ -0,0 +1,48 @@
+#include <cstring>
+#include <iostream>
+
+/**
+* Function which swaps two characters
+* @params {char} x - First character
+* @params {char} y - Second character
+*/
+void swap(char* x, char* y) {
+
+	char temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+/**
+* Function which prints all the permutations of a given string
+* @params {char array} c - Given string
+* @params {int} l - Lower index of the string
+* @params {int} r - Upper index of the string
+*/
+void printPermutationsString(char* c, int l, int r) {
+
+	if (l == r) {
+		std::cout << c << std::endl;
+	}
+	else {
+		for (int i = l; i <= r; i++) {
+			swap(c + l, c + i);
+			printPermutationsString(c, l + 1, r);
+			swap(c + l, c + i);							// Backtracking
+		}
+	}
+}
+
+/**
+* Starting point of the program
+*/
+int main() {
+
+	char c[] = "ABC";
+	int n = strlen(c);
+	printPermutationsString(c, 0, n - 1);
+
+	char c2[] = "ABCD";
+	int n2 = strlen(c2);
+	printPermutationsString(c2, 0, n2 - 1);
+}
