c326b2687d43645d74e9ba0e3b5b6a8d28879949
Akshay Sawant <asawant@gatech.edu>
2016-12-03 16:56:25 -0800

    Start knights tour

---
+++ G4G/Algo/Backtracking/02_KnightsTour.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#define N 8
+
+bool solveKnightsTourUtil() {
+	
+}
+
+bool solveKnightsTour() {
+
+}
+
+/**
+ * Starting point of the program
+*/
+int main() {
+
+	solveKnightsTour();
+	return 0;
+}
